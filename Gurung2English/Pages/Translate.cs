905cea962bd4f30bbe062d0941c9b26153b28167
roshan.a.gurung@outlook.com <roshan.a.gurung@outlook.com>
2023-04-16 16:00:43 -0400

    Add project files.

---
+++ Gurung2English/Pages/Translate.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+
+namespace Gurung2English.Pages
+{
+    public class Translate
+    {
+        public string GurungKey { get; set; }
+        public string EnglishValue { get; set; }
+        public string NepaliValue { get; set; }
+        public string Description { get; set; }
+        public string PronunciationKey { get; set; }
+
+         public string PronunciationLink { get; set; }
+        //public string PronunciationLink { get; set; } = "https://translate.google.com/?tl=ne&text=tamu%20ky%C4%AB&op=pronunciation";
+    }
+}
